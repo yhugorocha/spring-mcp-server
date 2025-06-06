08b49672c5e35bb46259ca4848ba035792765c8c
Hugo Silva <jose.silva@extreme.digital>
2025-06-06 15:49:59 -0300

    feat: another way to add ToolsCallBack.

--- src/main/java/br/com/yhugorocha/config/ToolConfig.java
+++ src/main/java/br/com/yhugorocha/config/ToolConfig.java
@@ -1,20 +1,25 @@
 package br.com.yhugorocha.config;
 
 import br.com.yhugorocha.tools.SolicitationService;
-import org.springframework.ai.support.ToolCallbacks;
-import org.springframework.ai.tool.ToolCallback;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
 
 @Configuration
 public class ToolConfig {
 
+    /*
     @Bean
     public List<ToolCallback> solicitationTools(SolicitationService solicitationService) {
         return List.of(
                 ToolCallbacks.from(solicitationService)
         );
+    }
+    */
+    @Bean
+    public ToolCallbackProvider solicitationTools(SolicitationService solicitationService) {
+        return MethodToolCallbackProvider.builder().toolObjects(solicitationService).build();
     }
 }
